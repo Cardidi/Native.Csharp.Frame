2ff98b43909119929804e2277e7938488d3a3a59
kotone <kotone@email.com>
2020-01-20 12:32:59 +0800

    upgrade sdk 0119

--- Native.Csharp.Sdk/Cqp/Model/StrangerInfo.cs
+++ Native.Csharp.Sdk/Cqp/Model/StrangerInfo.cs
@@ -53,7 +53,7 @@ namespace Native.Csharp.Sdk.Cqp.Model
 			using (BinaryReader reader = new BinaryReader (new MemoryStream (cipherBytes)))
 			{
 				this.QQ = new QQ (api, reader.ReadInt64_Ex ());
-				this.Nick = reader.ReadString_Ex ();
+				this.Nick = reader.ReadString_Ex (CQApi.DefaultEncoding);
 				this.Sex = (QQSex)reader.ReadInt32_Ex ();
 				this.Age = reader.ReadInt32_Ex ();
 			}
@@ -92,8 +92,8 @@ namespace Native.Csharp.Sdk.Cqp.Model
 		public override string ToString ()
 		{
 			StringBuilder builder = new StringBuilder ();
-			builder.AppendLine (string.Format ("帐号: {0}", this.QQ));
-			builder.AppendLine (string.Format ("昵称: {0}", this.Nick));
+			builder.AppendLine (string.Format ("帐号: {0}", this.QQ != null ? this.QQ.Id.ToString () : string.Empty));
+			builder.AppendLine (string.Format ("昵称: {0}", this.Nick != null ? this.Nick : string.Empty));
 			builder.AppendLine (string.Format ("性别: {0}", this.Sex));
 			builder.AppendFormat ("年龄: {0}", this.Age);
 			return builder.ToString ();
